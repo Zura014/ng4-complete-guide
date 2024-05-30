0beaf6868ccfe3360880b98a0fb54c71ce116293
Zura156 <117541032+Zura014@users.noreply.github.com>
2024-05-30 20:21:49 +0400

    fix

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
-import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-shopping-list',
@@ -11,20 +12,22 @@ import { Subscription } from 'rxjs';
 })
 export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
-  private subscription: Subscription;
+  private igChangeSub: Subscription;
 
-  constructor(private slService: ShoppingListService) { }
-  
+  constructor(private slService: ShoppingListService) {
+  }
+
   ngOnInit() {
     this.ingredients = this.slService.getIngredients();
-    this.subscription = this.slService.ingredientsChanged
-    .subscribe(
-      (ingredients: Ingredient[]) => {
-        this.ingredients = ingredients;
-      }
-    );
+    this.igChangeSub = this.slService.ingredientsChanged
+      .subscribe(
+        (ingredients: Ingredient[]) => {
+          this.ingredients = ingredients;
+        }
+      );
   }
+
   ngOnDestroy(): void {
-    this.subscription.unsubscribe();
+    this.igChangeSub.unsubscribe();
   }
 }
