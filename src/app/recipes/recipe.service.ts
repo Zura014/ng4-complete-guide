e8d91bd6874d3416221eae147eb5119b66ce39e2
Zura156 <117541032+Zura014@users.noreply.github.com>
2024-05-30 18:37:56 +0400

    Observables

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -4,9 +4,9 @@ import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+
 @Injectable()
 export class RecipeService {
-  recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
     new Recipe(
