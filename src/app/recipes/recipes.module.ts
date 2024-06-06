b7d8943300c288425202a42df4b035c9bbd54b5a
Zura156 <117541032+Zura014@users.noreply.github.com>
2024-06-06 23:07:32 +0400

    modules

---
+++ src/app/recipes/recipes.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from "@angular/core";
+import { RecipesComponent } from "./recipes.component";
+import { RecipeListComponent } from "./recipe-list/recipe-list.component";
+import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
+import { RecipeItemComponent } from "./recipe-list/recipe-item/recipe-item.component";
+import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
+import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
+import { RouterModule } from "@angular/router";
+import { CommonModule } from "@angular/common";
+import { ReactiveFormsModule } from "@angular/forms";
+import { RecipesRoutingModule } from "./recipes-routing.module";
+
+@NgModule({
+    declarations: [
+        RecipesComponent,
+        RecipeListComponent,
+        RecipeDetailComponent,
+        RecipeItemComponent,
+        RecipeEditComponent,
+        RecipeStartComponent,
+        ],
+    exports: [
+        RecipesComponent,
+        RecipeListComponent,
+        RecipeDetailComponent,
+        RecipeItemComponent,
+        RecipeEditComponent,
+        RecipeStartComponent,
+    ],
+    providers: [
+
+    ],
+    imports: [
+        RouterModule,
+        CommonModule,
+        ReactiveFormsModule,
+        RecipesRoutingModule        
+    ]
+})
+
+export class RecipesModule {
+
+}
