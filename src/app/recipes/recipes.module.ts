e8be9ed9951e22394d2a7567103061d1dee22b97
Zura156 <117541032+Zura014@users.noreply.github.com>
2024-06-07 20:28:47 +0400

    modules + lazy loading

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -9,6 +9,7 @@ import { RouterModule } from "@angular/router";
 import { CommonModule } from "@angular/common";
 import { ReactiveFormsModule } from "@angular/forms";
 import { RecipesRoutingModule } from "./recipes-routing.module";
+import { SharedModule } from "../shared/shared.module";
 
 @NgModule({
     declarations: [
@@ -34,7 +35,8 @@ import { RecipesRoutingModule } from "./recipes-routing.module";
         RouterModule,
         CommonModule,
         ReactiveFormsModule,
-        RecipesRoutingModule        
+        RecipesRoutingModule,
+        SharedModule   
     ]
 })
 
