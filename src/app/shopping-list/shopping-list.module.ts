7fff10f42a3134895a611e21ffda2db5c2a55f51
Zura156 <117541032+Zura014@users.noreply.github.com>
2024-06-08 22:10:58 +0400

    services

--- src/app/shopping-list/shopping-list.module.ts
+++ src/app/shopping-list/shopping-list.module.ts
@@ -5,6 +5,7 @@ import { RouterModule } from "@angular/router";
 import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
 import { SharedModule } from "../shared/shared.module";
+import { LoggingService } from "../logging.service";
 
 @NgModule({
     declarations: [
@@ -17,7 +18,10 @@ import { SharedModule } from "../shared/shared.module";
             { path: '', component: ShoppingListComponent },
         ]),
         SharedModule
-    ]
+    ],
+    // providers: [
+    //     LoggingService
+    // ]
 })
 
 export class ShoppingListModule {
